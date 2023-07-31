a577ed2330e095464caca91381dab846181a11ee
React App <yusufsaifymurtaza@gmail.com>
2023-07-31 12:07:02 +0530

    Count Words

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -7,6 +7,12 @@ export default function Form(props) {
         settext(textlocal);
         
     }
+    const  handleonclickLower=()=>{
+        let textlocal=text.toLowerCase();
+        settext(textlocal);
+   
+    }
+       
     const  handlechange=(event)=>{
         settext(event.target.value);
         
@@ -15,13 +21,25 @@ export default function Form(props) {
   return (
     
     <div>
-      <div>
-        <h1>{props.heading}</h1>
-      </div>
-      <div className="mb-3">
-        <textarea className="form-control" id="mybox" onChange={handlechange} value={text} rows="9"></textarea>
-      </div>
-      <button className="btn btn-primary" onClick={handleonclick}>Convert to Upper Case</button>
+       <div className="container">
+         <div >
+           <h1>{props.heading}</h1>
+         </div>
+         <div className="mb-3">
+           <textarea className="form-control" id="mybox" onChange={handlechange} value={text} rows="6"></textarea>
+         </div>
+            <button className="btn btn-primary" onClick={handleonclick}>Convert to Upper Case</button>
+            <button className="btn btn-primary mx-4" onClick={handleonclickLower}>Convert to Lower Case</button>
+        </div> 
+        <div className="container my-3">
+            <h2>Your Text Summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters </p>
+            <p>{0.008*text.split(" ").length} time to read </p>
+        </div>
+        <div className="container my-3">
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
     </div>
   )
-}
+  }
